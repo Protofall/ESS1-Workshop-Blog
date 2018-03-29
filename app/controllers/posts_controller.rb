db9fedd8c8438b1cd07f54056c00885afd919cd5
David Harley <david74437@gmail.com>
2018-03-29 13:02:35 +1030

    BANANA RETORDED (But really error checking on new post)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,26 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all.order('created_at DESC')
   	end
+
 	def new
+  		@post = Post.new
 	end
 
 	def show
   		@post = Post.find_by_id(params[:id])
   	end
-
+ 
 	def create
-	  @post = Post.new(post_params)
-	  @post.save
-	  redirect_to @post
-	end
-	private
-	def post_params
-	  params.require(:post).permit(:title, :body)
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
+	private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
